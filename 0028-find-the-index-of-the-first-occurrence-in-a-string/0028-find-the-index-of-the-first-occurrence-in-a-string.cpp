506f29621729ca4722d67ae231759547b5b84f46
Nilansha Bisht <114083286+Nilansha18@users.noreply.github.com>
2023-08-30 18:35:46 +0530

    Time: 0 ms (100.00%), Space: 6.3 MB (26.49%) - LeetHub

--- 0028-find-the-index-of-the-first-occurrence-in-a-string/0028-find-the-index-of-the-first-occurrence-in-a-string.cpp
+++ 0028-find-the-index-of-the-first-occurrence-in-a-string/0028-find-the-index-of-the-first-occurrence-in-a-string.cpp
@@ -7,12 +7,10 @@ public:
                 ans=j;
                 for(int i=1;i<needle.length();i++){
                     if(needle[i]!=haystack[j+i]) {
-                        cout<<ans<<" ";
                         ans=-1;
                         break;
                     }
                     else continue;
-                    
                 }
                 if(ans!=-1) return ans;
             }
