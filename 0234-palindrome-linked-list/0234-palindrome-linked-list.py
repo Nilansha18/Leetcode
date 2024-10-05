838579f8edd5533aafc08230e1d3a8aef44921dd
Nilansha Bisht <114083286+Nilansha18@users.noreply.github.com>
2024-10-05 22:44:59 +0530

    Time: 281 ms (77.77%), Space: 32.1 MB (67.36%) - LeetHub

--- 0234-palindrome-linked-list/0234-palindrome-linked-list.py
+++ 0234-palindrome-linked-list/0234-palindrome-linked-list.py
@@ -5,47 +5,26 @@
 #         self.next = next
 class Solution:
     def isPalindrome(self, head: Optional[ListNode]) -> bool:
-        if(head.next is None): return True
-        if(head.next.next is None): 
-            if(head.val==head.next.val): return True
-            else : return False
-        if(head.next.next.next is None):
-            if(head.val==head.next.next.val): return True
-            else : return False
+        
         slow=head
         fast=head
-        temp=ListNode()
         
-        while(fast.next is not None and fast.next.next is not None):
+        while(fast and fast.next):
             slow=slow.next
             fast=fast.next.next
-            
-        print(fast)
-        
-        #oddcase
-        if(fast.next is None):
-            temp.val=slow.val
-            temp.next=slow.next
-            slow.next=None
-            
-        else:
-            temp=slow.next
-            slow.next=None
-            
         
         prev=None
-        while(head is not None ):
-            temp2=head.next
-            head.next=prev
-            prev=head
-            head=temp2
-        
-        while(temp is not None or prev is not None):
-            if(temp.val!=prev.val): return False
-            else: 
-                temp=temp.next
-                prev=prev.next
-        
+        while slow:
+            front=slow.next
+            slow.next=prev
+            prev=slow
+            slow=front
+        left,right=head,prev
+        while right:
+            if left.val!=right.val:
+                return False
+            left=left.next
+            right=right.next
         return True
         
         
